4699bfbf4c3ff5f695e8f46160d3def41a323fb2
Kyllien <kyllien.romand@gmail.com>
2021-11-16 14:53:14 +0100

    MLP

--- DescentGradientBoost.py
+++ DescentGradientBoost.py
@@ -6,14 +6,57 @@ import GestionData as gd
 
 
 def GradientBoost(train,action):
+
+    #Selection d'une base plus petite car trop grande pour effectuer les calculs au vu de la puissance de notre ordinateur
+    #On selectionne sur les 50 premiers jours pour avoir mini 100k d'observations, ce qui correspond à 1/10 de la base
+
+
+    df2 = df[df['date']<=49]
+
+    train, action = pr_action(df2)
+
     #Random Cross Validation
     X_train, X_test, y_train, y_test = skms.train_test_split(train, action, test_size=0.3, random_state=0)
 
     #GradientBoostingClassifier
+
     import time
     start_time = time.time()
-    clf = ske.GradientBoostingClassifier(n_estimators=500, learning_rate=1.0, max_depth=1, random_state=0).fit(X_train, y_train)
+    clf = ske.GradientBoostingClassifier(n_estimators=2000, learning_rate=1.0, max_depth=7, random_state=0).fit(X_train, y_train)
     print("--- %s seconds ---" % (time.time() - start_time))
-    
+    print("Accuracy score (training): {0:.3f}".format(clf.score(X_train, y_train)))
+    print("Accuracy score (validation): {0:.3f}".format(clf.score(X_test, y_test)))
+    #Resultat pour : n_estimators=500, learning_rate=1.0, max_depth=1, random_state=0
+    #Accuracy score (training): 0.569
+    #Accuracy score (validation): 0.554
+
+    #Resultat pour : n_estimators=1000, learning_rate=1.0, max_depth=1, random_state=0
+    #Accuracy score (training): 0.577
+    #Accuracy score (validation): 0.556
+
+    #Resultat pour : n_estimators=500, learning_rate=1.0, max_depth=3, random_state=0
+    #Accuracy score (training): 0.70
+    #Accuracy score (validation): 0.62
+
+    #Resultat pour : n_estimators=500, learning_rate=3.0, max_depth=1, random_state=0
+    #Accuracy score (training): 0.471
+    #Accuracy score (validation): 0.475
+
+    #Resultat pour : n_estimators=500, learning_rate=0.8, max_depth=1, random_state=0
+    #Accuracy score (training): 0.566
+    #Accuracy score (validation): 0.553
+
+    #Resultat pour : n_estimators=500, learning_rate=0.5, max_depth=1, random_state=0
+    #Accuracy score (training): 0.561
+    #Accuracy score (validation): 0.551
+
+    #Resultat pour : n_estimators=500, learning_rate=1, max_depth=7, random_state=0
+    #Accuracy score (training): 0.985
+    #Accuracy score (validation): 0.663
+
+    #Resultat pour : n_estimators=500, learning_rate=1, max_depth=5, random_state=0
+    #Accuracy score (training): 0.855
+    #Accuracy score (validation): 0.647
+
     return clf
 
---
+++ MLP.py
@@ -0,0 +1,44 @@
+import pandas as pd
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.model_selection import cross_val_score
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import StratifiedKFold
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+from sklearn import model_selection as skms
+
+
+def MLP(df) :
+
+    df2 = df[df['date']<=49]
+
+    train, action = pr_action(df2)
+
+    # encode class values as integers
+    encoder = LabelEncoder()
+    encoder.fit(action)
+    encoded_Y = encoder.transform(action)
+
+
+    # baseline model
+    def create_baseline():
+	    # create model
+	    model=Sequential()
+	    model.add(Dense(130, input_dim=130, activation='relu'))
+	    model.add(Dense(1, activation='sigmoid'))
+	    # Compile model
+	    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+	    return model
+    # evaluate model with standardized dataset
+    estimator = KerasClassifier(build_fn=create_baseline, epochs=100, batch_size=5, verbose=0)
+    kfold = StratifiedKFold(n_splits=10, shuffle=True)
+    results = cross_val_score(estimator, train, encoded_Y, cv=kfold)
+    print("Baseline: %.2f%% (%.2f%%)" % (results.mean()*100, results.std()*100))
+
+    #Avec build_fn=create_baseline, epochs=100, batch_size=5, verbose=0 et n_splits = 10 
+    #Resultat 57.4%, sd = 0.24%
+
+
+    return model
